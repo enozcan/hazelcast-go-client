1a8d924266b3bb3c29ebb400ab06d6fa151b462b
Enes Ozcan <enes.ozcan@hazelcast.com>
2021-09-23 16:41:46 +0300

    Add invalidation listener.

--- internal/nearcache/near_cache.go
+++ internal/nearcache/near_cache.go
@@ -1,5 +1,13 @@
 package nearcache
 
+import (
+	"github.com/hazelcast/hazelcast-go-client"
+	"github.com/hazelcast/hazelcast-go-client/internal/proto"
+	"github.com/hazelcast/hazelcast-go-client/internal/proto/codec"
+	"github.com/hazelcast/hazelcast-go-client/internal/serialization"
+	"github.com/hazelcast/hazelcast-go-client/types"
+)
+
 type NearCache struct {
 	nearCacheRecordStore RecordStore
 	serializeKeys        bool
@@ -15,4 +23,23 @@ func (n *NearCache) tryReserveForUpdate(key interface{}) int64 {
 
 func (n *NearCache) tryPublishReserved(key interface{}, data []byte, reservationID int64) interface{} {
 	return n.nearCacheRecordStore.tryPublishReserved(key, data, reservationID)
+}
+
+func (n *NearCache) newInvalidationListener(name string, localOnly bool) (subsID types.UUID, add *proto.ClientMessage, remove *proto.ClientMessage, handler func(clientMessage *proto.ClientMessage)) {
+	// TODO: to be registered in proxy.listenerBinder
+	subsID = types.NewUUID()
+	add = codec.EncodeMapAddNearCacheInvalidationListenerRequest(name, int32(hazelcast.EntryInvalidated), localOnly)
+	remove = codec.EncodeMapRemoveEntryListenerRequest(name, subsID)
+	handler = func(clientMessage *proto.ClientMessage) {
+		codec.HandleMapAddNearCacheInvalidationListener(clientMessage, n.handleIMapInvalidationEvent, n.handleIMapBatchInvalidationEvent)
+	}
+	return
+}
+
+func (n *NearCache) handleIMapInvalidationEvent(key *serialization.Data, sourceUuid types.UUID, partitionUuid types.UUID, sequence int64) {
+	// TODO: invalidate single key in the store
+}
+
+func (n *NearCache) handleIMapBatchInvalidationEvent(keys []*serialization.Data, sourceUuids []types.UUID, partitionUuids []types.UUID, sequences []int64) {
+	// TODO: invalidate all keys in the store
 }
